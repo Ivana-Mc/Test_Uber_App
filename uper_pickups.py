bb023ad20087cb3c2d999902a3c127f001b2db6e
Ivana-Mc <sba23216@student.cct.ie>
2025-05-13 18:56:59 +0100

    Update uper_pickups.py

--- uper_pickups.py
+++ uper_pickups.py
@@ -11,11 +11,10 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-dat
 def load_data(nrows):
 	data=pd.read_csv(DATA_URL, nrows=nrows)
 	lowercase=lambda x: str(x).tolower()
-	data.rename(lowercase, axis='columns', inplace=True)
+	data.rename(lowercase, axis="columns", inplace=True)
 	data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
 	return data
 
-data_load_state=st.text('Loading Data....')
-
+data_load_state=st.text("Loading Data...")
 data=load_data(10000)
-data_load_state.text('Loading Data....done!')
+data_load_state.text("Loading Data...done!")
